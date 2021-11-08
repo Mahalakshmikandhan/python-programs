0a0e32f5952a275ed5892145c2f5fed966e17b0b
Mahalakshmikandhan <93925692+Mahalakshmikandhan@users.noreply.github.com>
2021-11-08 19:23:18 +0530

    filename
    this is a python program to find the extension of a file whether it is a c++ program or a python program or a text file

---
+++ filename.py
@@ -0,0 +1,12 @@
+#filname and extension
+filename=str(input("Enter the filename: "))
+if("py"in filename):
+    print("The extension is python")
+elif("cpp"in filename):
+    print("The extension is c++")
+elif("txt" in filename):
+    print("The extension is text")
+else:
+    print("Extension is not mentioned")
+print("End of the program")
+    

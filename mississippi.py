96772c46e6a17f340b8b7414e36a2292f121e339
Mahalakshmikandhan <93925692+Mahalakshmikandhan@users.noreply.github.com>
2021-11-18 11:34:19 +0530

    Mississippi python program
    This is a python program which finds the number of occurence of the letters in the word mississippi

---
+++ mississippi.py
@@ -0,0 +1,24 @@
+#mississippi program
+word="MISSISSIPPI"
+count_M=0
+count_I=0
+count_S=0
+count_P=0
+for i in word:
+    if i=="M":
+        count_M=count_M+1
+    elif i=="I":
+        count_I=count_I+1
+    elif i=="S":
+        count_S=count_S+1
+    elif i=="P":
+        count_P=count_P+1
+print("THE WORD IS MISSISSIPPI")
+print("Occurence of M : ",count_M)
+print("Occurence of I : ",count_I)
+print("Occurence of S : ",count_S)
+print("Occurence of P : ",count_P)
+
+
+        
+        
